77ca7967586ca233053148bd3f5f812869351e3d
Keviinplz <kevin.pinochet@ing.uchile.cl>
2020-12-23 01:07:03 -0300

    Documentary

--- lib/ast/visitor.py
+++ lib/ast/visitor.py
@@ -1,18 +1,75 @@
+"""
+Visitor Module
+
+Module that has all Visitor to AST.
+
+"""
+
+
 from .operations import Sum, Mul, Div, Sub, BinaryOperator
 from .expresion import Constant, Variable
 
 class Visitor():
+    """
+    Class Visitor
+
+    This class is parent of all visitors classes bassed on AST.
+    """
 
     def __init__(self, result):
+        """
+        Class Visitor
+
+        This class is parent of all visitor classes bassed on AST.
+
+        Parameters:
+        
+        result: any
+            Result after visit an AST structure
+            Can be any type, depends of the objetive of the visitor.
+        """
+
         self.result = result
 
-    def getResult(self):
+    def getResult(self) -> any:
+        """
+        getResult
+
+        Return result of Visitor
+
+        returntype: any
+            Result of visitor after visit an AST structure
+        """
+
         return self.result
     
-    def setResult(self, newValue):
+    def setResult(self, newValue: any) -> None:
+        """
+        setResult
+
+        Setter result.
+
+        """
+
         self.result = newValue
     
     def visitBinaryOperator(self, op: BinaryOperator, function) -> None:
+        """
+        visitBinaryOperator
+
+        Handle a Binary Operator visit, updating result with
+        a function that depends of operator.
+
+        Parameters:
+
+        op: BinaryOperator
+            A binary Operator object
+
+        function: function
+            Function that apply to new value, to set result.
+        """
+
+
         op.getLeft().accept(self)
         leftValue = self.getResult()
 
@@ -65,11 +122,13 @@ class EvaluateExpresionVisitor(Visitor):
         self.visitBinaryOperator(d, lambda x, y : x / y if y != 0 else 1)
    
     def visitVariable(self, v: Variable) -> None:
-        try:
-            value = self.variableSet[v.getValue()]
-            self.setResult(float(value))
-        except KeyError:
-            raise KeyError(f"Free ocurrency of {v.getValue()} in AST\nPlease declare it when initialice this visitor.")
+        if v.getValue() not in self.variableSet.keys():
+            raise KeyError(f"Free ocurrency of {v.getValue()}. Please declare it in Visitor Constructor")
+        value = self.variableSet[v.getValue()]
+        if type(value) != float or type(value) != int:
+            raise SyntaxError(f"{value} must to be Float or Integer, {type(value)} is not allowed")
+
+        self.setResult(value)
         
     def visitConstant(self, c: Constant) -> None:
         self.setResult(float(c.getValue()))
