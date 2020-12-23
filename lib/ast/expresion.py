8456406d8892315c4bdc8757a3b91d98fdde36dd
Keviinplz <kevin.pinochet@ing.uchile.cl>
2020-12-23 02:25:00 -0300

    Adding docstring
    To expresion module and operations module

--- lib/ast/expresion.py
+++ lib/ast/expresion.py
@@ -1,43 +1,166 @@
+"""
+Module Expresion
+
+Module that create Abstracts expresion to AST
+"""
 from abc import ABC, abstractmethod
 
 class Expresion(ABC):
+    """
+    Abstract Class Expresion
 
+    Abstract representation of being an expresion
+
+    """
     def __init__(self, value: any):
+        """
+        Abstract Class Expresion
+
+        Abstract representation of being an expresion
+
+        Parameter:
+        
+            value: any
+                Value of the expresion
+        """
         self.value = value
 
     @abstractmethod
-    def accept(self, visitor):
+    def accept(self, visitor) -> None:
+        """
+        ABSTRACT METHOD
+        accept
+
+        Accept an visitor, a set this object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         pass
 
-    def getValue(self):
+    def getValue(self) -> any:
+        """
+        getValue
+
+        Return value of the expresion
+
+        returntype: any
+            Value of the expresion
+        """
         return self.value
 
 class BinaryOperator(Expresion):
-    
+    """
+    Class BinaryOperator
+
+    Abstract a Binary Operator
+    """    
     def __init__(self, left: Expresion, right: Expresion):
+        """
+        Class BinaryOperator
+
+        Abstract a Binary Operator
+        Apply an operator to the tuple left and right
+        Parameters:
+
+            left: Expresion
+                Expresion object
+            
+            right: Expresion
+                Expresion object
+        
+        """
         self.left = left
         self.right = right
 
     def getLeft(self) -> Expresion:
+        """
+        getLeft
+
+        Return left expresion
+
+        returntype: Expresion
+            Left expresion of the operator
+        """
         return self.left
 
     def getRight(self) -> Expresion: 
+        """
+        getRight
+
+        Return right expresion
+
+        returntype: Expresion
+            Right expresion of the operator
+        """
         return self.right
 
 class Constant(Expresion):
+    """
+    Class Constant
+
+    Represent a constant value
+    """
 
     def __init__(self, value: float):
+        """
+        Class Constant
+
+        Represent a constant value
+
+        Parameters:
+            
+            value: float
+                Constant value
+        """
         super(Constant, self).__init__(value)
 
     def accept(self, visitor) -> None:
+        """
+        accept
+
+        Accept an visitor, a set a Constant object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         visitor.visitConstant(self)
 
 class Variable(Expresion):
+    """
+    Class Variable
+
+    Represent a Variable value
+    """
 
     def __init__(self, value: str):
+        """
+        Class Variable
+
+        Represent a variable value
+
+        Parameters:
+            
+            value: str
+                Variable value
+        """
         super(Variable, self).__init__(value)
 
     def accept(self, visitor) -> None:
+        """
+        accept
+
+        Accept an visitor, a set a Variable object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         visitor.visitVariable(self)
 
 
--- lib/ast/operations.py
+++ lib/ast/operations.py
@@ -1,33 +1,148 @@
+"""
+Module Operations
+
+Operations that can be used in AST.
+"""
 from .expresion import Expresion, BinaryOperator
 
 class Sum(BinaryOperator):
-    
+    """
+    Class Sum
+
+    Representation of sum of two values
+    """
     def __init__(self, left: Expresion, right: Expresion):
+        """
+        Class Sum
+
+        Representation of sum of two values
+
+        Parameters:
+
+            left: Expresion
+                Left expresion of the sum
+            
+            right: Expresion
+                Right expresion of the sum
+        """
         super(Sum, self).__init__(left, right)
 
     def accept(self, visitor):
+        """
+        accept
+
+        Accept an visitor, a set a Sum object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         visitor.visitSum(self)
 
 class Mul(BinaryOperator):
+    """
+    Class Mul
 
+    Representation of multiplication of two values
+    """
     def __init__(self, left: Expresion, right: Expresion):
+        """
+        Class Mul
+
+        Representation of multiplication of two values
+
+        Parameters:
+
+            left: Expresion
+                Left expresion of the multiplication
+            
+            right: Expresion
+                Right expresion of the multiplication
+        """
         super(Mul, self).__init__(left, right)
 
     def accept(self, visitor):
+        """
+        accept
+
+        Accept an visitor, a set a Mul object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         visitor.visitMul(self)
 
 class Div(BinaryOperator):
-    
+    """
+    Class Div
+
+    Representation of division of two values
+    """
+
     def __init__(self, left: Expresion, right: Expresion):
+        """
+        Class Div
+
+        Representation of division of two values
+
+        Parameters:
+
+            left: Expresion
+                Left expresion of the division
+            
+            right: Expresion
+                Right expresion of the division
+        """
         super(Div, self).__init__(left, right)
 
     def accept(self, visitor):
+        """
+        accept
+
+        Accept an visitor, a set a Div object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         visitor.visitDiv(self)
 
 class Sub(BinaryOperator):
+    """
+    Class Sub
+
+    Representation of substract of two values
+    """
 
     def __init__(self, left: Expresion, right: Expresion):
+        """
+        Class Sub
+
+        Representation of substract of two values
+
+        Parameters:
+
+            left: Expresion
+                Left expresion of the substract
+            
+            right: Expresion
+                Right expresion of the substract
+        """
         super(Sub, self).__init__(left, right)
 
     def accept(self, visitor):
+        """
+        accept
+
+        Accept an visitor, a set a Div object visited
+
+        Parameter:
+        
+            visitor: Visitor
+                A visitor
+        """
         visitor.visitSub(self)
